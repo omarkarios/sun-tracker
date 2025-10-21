fe966a221fcf2d5ca16e44ef127cc9538c79dc3c
omarkarios <markariosoketch@gmail.com>
2025-10-21 06:02:21 +0300

    Listed counties.

---
+++ script.js
@@ -0,0 +1,11 @@
+const counties = [
+  { name: "Nairobi", lat: -1.2864, lon: 36.8172 },
+  { name: "Makueni", lat: -1.803, lon: 37.6203 },
+  { name: "Machakos", lat: -1.5177, lon: 37.2634 },
+  { name: "Nakuru", lat: -0.3031, lon: 36.0800 },
+  { name: "Eldoret", lat: 0.5143, lon: 35.2698 },
+  { name: "Kisumu", lat: -0.0917, lon: 34.7679 },
+  { name: "Garissa", lat: -0.4569, lon: 39.6583 },
+  { name: "Nyeri", lat: -0.4197, lon: 36.9476 },
+  { name: "Kitui", lat: -1.3667, lon: 38.0167 }
+];
